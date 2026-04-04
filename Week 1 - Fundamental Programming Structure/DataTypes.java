d37d634c97dd412fabfe649b407970424c7bebea
Nida Atqiya <nida.atqiya.tif425@polban.ac.id>
2026-04-04 09:46:28 +0700

    Tugas TEKPROPR

---
+++ Week 1 - Fundamental Programming Structure/DataTypes.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number of test cases : ");
+        int angka = input.nextInt();
+        
+        for (int i = 0; i < angka; i++) {
+            System.out.print("Enter number : ");
+
+            try{
+                long n = input.nextLong();
+                System.out.println(n + " Can be fitted in : ");
+
+                if (n >= Byte.MIN_VALUE && n <= Byte.MAX_VALUE)
+                    System.out.println("* byte");
+
+                if (n >= Short.MIN_VALUE && n <= Short.MAX_VALUE)
+                    System.out.println("* short");
+
+                if (n >= Integer.MIN_VALUE && n <= Integer.MAX_VALUE)
+                    System.out.println("* integer");
+
+                if (n >= Long.MIN_VALUE && n <= Long.MAX_VALUE)
+                    System.out.println("* long");
+
+            } catch (Exception e) {
+                System.out.println(" Can't be fitted in everywhere.");
+
+                input.next();
+            }
+        } 
+        input.close();
+    }
+}
